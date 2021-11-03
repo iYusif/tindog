ffa0bbb69429f42805d5f3f38ab8b306bde77e54
iYusif <ysibrahimli@gmail.com>
2021-11-04 00:45:06 +0400

    a

--- script.js
+++ script.js
@@ -261,7 +261,7 @@
 //         output = [0, 1];
 
 //         for (var i = 2; i < n; i++) {
-//             output.push(output[output.length - 2] + output[output.length - 1])
+//             output.push(output[output.length - 2] + output[output.length - 1]);
 
 //         }
 //     }
